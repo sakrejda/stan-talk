33b8b6dcba344150399c11b1726cd9e25e24f7e5
Krzysztof Sakrejda <krzysztof@toothless.ddns.umass.edu>
2015-10-13 09:02:11 -0400

    Working Metropolis sampler for the donut model.

--- donut-sim.R
+++ donut-sim.R
@@ -18,7 +18,7 @@ r_bivariate_donut <- function(n, r_mu=1, r_sd=r_mu/4) {
 }
 
 d_bivariate_donut <- function(x, y, r_mu=1, r_sd=r_mu/4, log=TRUE) {
-  r <- sqrt(x^2,y^2)
+  r <- sqrt(x^2+y^2)
 #	theta <- asin(y/r)
 # dunif(theta, 0,2*pi)
   cds <- dnorm(r,r_mu,r_sd,log=log)
@@ -28,8 +28,11 @@ d_bivariate_donut <- function(x, y, r_mu=1, r_sd=r_mu/4, log=TRUE) {
 		return(prod(cds))
 }
 
+r_mu = 5
+r_sd = 1
+
 library(ggplot2)
-draw_donut <- data.frame(r_bivariate_donut(10^4, 2,2/6))
+draw_donut <- data.frame(r_bivariate_donut(10^3, r_mu, r_sd))
 
 pl_sim <- ggplot(data=draw_donut, aes(x=x, y=y)) + 
 	geom_point() + geom_density2d(adjust=2) +
@@ -41,10 +44,78 @@ pl_sim_only_density <- ggplot(data=draw_donut, aes(x=x, y=y)) +
 
 print(pl_sim)
 print(pl_sim_only_density)
-print(cov(draw_donut))
+
 
 #' What does Gibbs do?
+#' Later 
+ 
+
 #' What does Metropolis do?
 
+#' Proposal p(x,y|x*,y*)
+r_metropolis_proposal <- function(xy, sd) {
+  xy <- c(x=rnorm(1,xy['x'],sd), y=rnorm(1,xy['y'],sd))
+  return(xy)
+}
+
+d_metropolis_proposal <- function(xy_star, xy, sd, log=TRUE) {
+  d_xy <- c(dnorm(xy_star['x'],xy['x'],sd,log), dnorm(xy_star['y'],xy['y'],sd,log))
+  if (isTRUE(log))
+    d_xy <- sum(d_xy)
+  else
+    d_xy <- prod(d_xy)
+  return(d_xy)
+}
+
+
+metropolis_acceptance <- function(xy, xy_star, theta, sd_proposal) {
+  log_d_xy <- d_bivariate_donut(xy['x'], xy['y'], theta['r_mu'], theta['r_sd'], log=TRUE)
+  log_d_xy_star <- d_bivariate_donut(xy_star['x'], xy_star['y'], theta['r_mu'], theta['r_sd'], log=TRUE)
+  log_d_xy_proposal <- d_metropolis_proposal(xy, xy_star, sd_proposal, log=TRUE)
+  log_d_xy_star_proposal <- d_metropolis_proposal(xy_star, xy, sd_proposal, log=TRUE)
+  a_ratio <- exp((log_d_xy_star + log_d_xy_proposal) - (log_d_xy + log_d_xy_star_proposal))
+  a_prob <- ifelse(a_ratio > 1, 1, a_ratio)
+  return(a_prob)
+}
+
+donut_metropolis <- function(n_iter, xy_init, theta, sd_proposal) {
+  samples <- matrix(data=NA, nrow=6, ncol=n_iter+1)
+  rownames(samples) <- c('iter','x','y','r','s','log_d')
+  r <- sqrt(xy_init['x']^2+xy_init['y']^2)
+  s <- acos(xy_init['x']/r)
+  samples[,1] <- c(1,xy_init['x'], xy_init['y'], r, s, d_bivariate_donut(xy_init['x'],xy_init['y'], theta['r_mu'], theta['r_sd'], log=TRUE))
+  xy <- xy_init
+  for(i in 2:n_iter+1) {
+    xy_star <- r_metropolis_proposal(xy, sd_proposal)
+    a_prob <- metropolis_acceptance(xy, xy_star, theta, sd_proposal)
+    if (a_prob > runif(1)) {
+      xy <- xy_star
+    }
+    r <- sqrt(xy['x']^2+xy['y']^2)
+    s <- acos(xy['x']/r)
+    samples[,i] <- c(i, xy['x'],xy['y'],r,s,d_bivariate_donut(xy['x'], xy['y'], theta['r_mu'], theta['r_sd'], log=TRUE)) 
+  }
+  return(samples)
+}
+
+#' sample:
+o <- donut_metropolis(1000, c(x=-5, y=0), c(r_mu=r_mu,r_sd=r_sd),1) %>% t %>% data.frame
+
+#' define a x/y grid:
+a_grid <- expand.grid(x=seq(-8,8,l=50), y=seq(-8,8,l=50)) %>% data.frame
+a_grid[['log_d']] <- apply(a_grid, 1, function(row) d_bivariate_donut(row['x'], row['y'],r_mu, r_sd,log=FALSE))
+
+pl_sad_metropolis <- ggplot() + 
+  geom_contour(data=a_grid, aes(x=x, y=y, z=log_d)) + 
+  geom_path(data=o, aes(x=x, y=y, colour=log_d)) + 
+  geom_point(data=o, aes(x=x, y=y)) + theme_minimal() + 
+  coord_cartesian(xlim=c(-10,10), ylim=c(-10,10))
+
+pl_sad_metropolis_trace <- ggplot(data=o %>% gather(parameter, value, x, y, r, s, log_d), aes(x=iter, y=value)) +
+  facet_grid( parameter ~ ., scales='free_y') + geom_line() + theme_minimal() + 
+  theme(strip.text.y=element_text(angle=0))
+
+print(pl_sad_metropolis)
+print(cov(draw_donut))
 
 
